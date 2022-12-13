3d410505526be4e7958857612e85bc2b6233f5c1
Hudson Retonde <hudsonsretonde@gmail.com>
2022-12-12 21:48:55 -0300

    Meu primeiro objeto com dart.

--- Dart criando e manipulando variáveis e listas/index.dart
+++ Dart criando e manipulando variáveis e listas/index.dart
@@ -1,3 +1,12 @@
-void main(List<String> arguments) {
-  print('Hello Word!');
+void main() {
+  int idade = 36;
+  double altura = 1.84;
+  bool lerAPalavraEBom = true;
+  String nome = 'Hudson';
+  String apelido = 'Cuca';
+  String frase = 'Olá, meu nome é $nome \n'
+      'tenho $idade anos, \n'
+      'minhaaltura é $altura, \n'
+      'alguns parentes me chamam de $apelido.';
+  print(frase);
 }
