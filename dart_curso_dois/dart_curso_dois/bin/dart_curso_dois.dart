268888133bd1911fefd398b95feb9c287b0bd62a
Hudson Retonde <hudsonsretonde@gmail.com>
2023-08-15 19:16:07 -0300

    inicio a poo

--- dart_curso_dois/dart_curso_dois/bin/dart_curso_dois.dart
+++ dart_curso_dois/dart_curso_dois/bin/dart_curso_dois.dart
@@ -1,3 +1,14 @@
 void main() {
-  print("Olá, mundo!");
+  String nome = "Laranja";
+  double peso = 100.2;
+  String cor = "Verde e Amarela";
+  String sabor = "Doce e cítrica";
+  int diasDeColheita = 40;
+  bool isMadura;
+
+  if (diasDeColheita >= 30) {
+    isMadura = true;
+  } else {
+    isMadura = false;
+  }
 }
