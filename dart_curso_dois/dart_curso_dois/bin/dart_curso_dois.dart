dbd6a331a2c6896d240c395b6930a7568de0ec3c
Hudson Retonde <hudsonsretonde@gmail.com>
2023-08-25 15:10:19 -0300

    Trabalhando com herança

--- dart_curso_dois/dart_curso_dois/bin/dart_curso_dois.dart
+++ dart_curso_dois/dart_curso_dois/bin/dart_curso_dois.dart
@@ -12,19 +12,22 @@ void main() {
   print(fruta02.estaMadura(10));
 }
 
-class Fruta {
-  String nome;
-  double peso;
-  String cor;
+class Fruta extends Alimento {
   String sabor;
   int DiasDeColheira;
   bool? isMadura;
-  Fruta(this.nome, this.peso, this.cor, this.sabor, this.DiasDeColheira,
-      {this.isMadura});
+  Fruta(String nome, double peso, String cor, this.sabor, this.DiasDeColheira,
+      {this.isMadura})
+      : super(nome, peso, cor);
+
   estaMadura(int diasMadura) {
     isMadura = DiasDeColheira >= diasMadura;
     print(
         "A sua $nome foi colhida a $DiasDeColheira dias, e precisa de $diasMadura para ser comida. Ela está madura? $isMadura.");
+  }
+
+  fazerSuco() {
+    print("Você fez um ótimo suco!");
   }
 }
 
@@ -33,14 +36,23 @@ class Alimento {
   double peso;
   String cor;
   Alimento(this.nome, this.peso, this.cor);
+
+  void printAlimento() {
+    print("Este(a) $nome pesa $peso grams e é $cor.");
+  }
 }
 
-class Legumes {
-  String nome;
-  double peso;
-  String cor;
+class Legumes extends Alimento {
   bool IsPrecisaCozinha;
-  Legumes(this.nome, this.peso, this.cor, this.IsPrecisaCozinha);
+  Legumes(String nome, double peso, String cor, this.IsPrecisaCozinha)
+      : super(nome, peso, cor);
+  void cozinhar() {
+    if (IsPrecisaCozinha) {
+      print("Prontp, o $nome já está conzinhando!");
+    } else {
+      print("Este legume nem precisa conzinhar...");
+    }
+  }
 }
 
 class Citricas {
